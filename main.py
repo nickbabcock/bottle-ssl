1ff3bfd6fea0264638ac0c1623068bab543b4b1e
Nick Babcock <nbabcock19@hotmail.com>
2013-01-16 19:09:50 -0500

    Add licensing info

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 """
 A simple example of using bottle to achieve an HTTPS login using Linux to
 verify username and password.
+
+Copyright (c) TotalCAE. All rights reserved.
+
+This code is published under the Microsoft Public License (Ms-PL).  A copy
+of the license should be distributed with the code. 
+
+THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 """
 
 from bottle import *

be319f52a77acf34e856c9c13546d8e9b9091614
Nick Babcock <nbabcock19@hotmail.com>
2014-12-16 17:41:29 -0500

    No need for header with license file

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-"""
-A simple example of using bottle to achieve an HTTPS login using Linux to
-verify username and password.
-
-Copyright (c) TotalCAE. All rights reserved.
-
-This code is published under the Microsoft Public License (Ms-PL).  A copy
-of the license should be distributed with the code. 
-
-THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-"""
-
 from bottle import (route, response, run, redirect, request, static_file,
                     ServerAdapter, default_app)
 from beaker.middleware import SessionMiddleware

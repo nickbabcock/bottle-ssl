a753bab27d882634bfb62633e198f0c1a428d9cc
Nick Babcock <nbabcock19@hotmail.com>
2017-09-26 07:00:00 -0400

    Add tests

---
+++ test_app.py
@@ -0,0 +1,43 @@
+from main import app, SSLCherryPyServer
+from bottle import run
+from multiprocessing import Process
+import requests
+import time
+import urllib3
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def run_app():
+    run(app=app, host='0.0.0.0', port=8443, server=SSLCherryPyServer)
+
+def setup_module(module):
+    module.proc = Process(target=run_app)
+    module.proc.start()
+
+def teardown_module(module):
+    module.proc.terminate()
+
+# Purpose of this test:
+#  - Not logged in by default
+#  - The log in mechanism works
+#  - After logging in, whoami returns the correct user (eg. session working)
+#  - After logging out, user is actually logged out
+def test_whoami():
+    s = requests.Session()
+    who = s.get('https://localhost:8443/whoami', verify=False).json()
+    assert who == { 'd': None }
+
+    creds = { 'UserName': 'BottleUser', 'Password': 'iambottle' }
+    login = s.post('https://localhost:8443/login', data=creds)
+    assert login.status_code == 200
+
+    who = s.get('https://localhost:8443/whoami', verify=False).json()
+    assert who == { 'd': 'BottleUser' }
+
+    logout = s.post('https://localhost:8443/logout')
+    assert logout.status_code == 200
+
+    who = s.get('https://localhost:8443/whoami', verify=False).json()
+    assert who == { 'd': None }
